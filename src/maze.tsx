04fad607e04e862a96e45a3ea4902d8323b71b74
prabesh <prabeshpathak50@gmail.com>
2025-02-13 20:29:49 +0545

    feat: added generate button

--- src/maze.tsx
+++ src/maze.tsx
@@ -1,34 +1,34 @@
-export const Gen = () => {
-  const max_size = 61;
-  const startRow = 1;
-  const startCol = 1;
+import { useState } from "react";
 
-  const direction = [
-    [0, -2],
-    [2, 0],
-    [0, 2],
-    [-2, 0],
-  ];
+const max_size = 51;
+const startRow = 1;
+const startCol = 1;
 
+const direction = [
+  [0, -2],
+  [2, 0],
+  [0, 2],
+  [-2, 0],
+];
+
+const isValidWall = (grid: Uint8Array<ArrayBuffer>, row: number, col: number) =>
+  row > 0 &&
+  col > 0 &&
+  row < max_size - 1 &&
+  col < max_size - 1 &&
+  grid[row * max_size + col] === 1;
+
+const generateMaze = () => {
   const frontier = new Set<string>();
   const grid = new Uint8Array(max_size * max_size).fill(1);
-
-  const isValidWall = (row: number, col: number) =>
-    row > 0 &&
-    col > 0 &&
-    row < max_size - 1 &&
-    col < max_size - 1 &&
-    grid[row * max_size + col] === 1;
-
   for (const [r, c] of direction) {
     const newRow = startRow + r;
     const newCol = startCol + c;
 
-    if (isValidWall(newRow, newCol)) {
+    if (isValidWall(grid, newRow, newCol)) {
       frontier.add(`${newRow},${newCol},${startRow},${startCol}`);
     }
   }
-
   while (frontier.size > 0) {
     const randomIndex = Math.floor(Math.random() * frontier.size);
     const [row, col, prevRow, prevCol] = [...frontier][randomIndex]
@@ -45,21 +45,40 @@ export const Gen = () => {
     for (const [r, c] of direction) {
       const newRow = row + r;
       const newCol = col + c;
-      if (isValidWall(newRow, newCol)) {
+      if (isValidWall(grid, newRow, newCol)) {
         frontier.add(`${newRow},${newCol},${row},${col}`);
       }
     }
   }
+  return grid;
+};
+
+export const Gen = () => {
+  const m = generateMaze();
+  const [maze, setMaze] = useState(m);
 
   return (
-    <div>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        gap: "20px",
+      }}
+    >
+      <button
+        style={{ alignSelf: "flex-start" }}
+        onClick={() => setMaze(generateMaze())}
+      >
+        Generate Maze
+      </button>
       <div
         style={{
           display: "grid",
           gridTemplateColumns: `repeat(${max_size}, 20px)`,
         }}
       >
-        {Array.from(grid).map((item, index) => (
+        {Array.from(maze).map((item, index) => (
           <span
             key={index}
             style={{
